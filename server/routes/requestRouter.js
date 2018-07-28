b9fe844a3f1e12a8d89f23df32805bb4e05d559c
Amanda Reilly <amandamichellereilly@gmail.com>
2018-07-28 14:07:44 -0700

    Update requestRouter to use RequestController

--- server/routes/requestRouter.js
+++ server/routes/requestRouter.js
@@ -1,29 +1,15 @@
 const express = require('express');
-const data = require('../../source/client-js/MOCK-DATA/mock-request-data');
+const RequestController = require('../controllers/RequestController');
 
 const router = express.Router();
-
 // List All Requests
-router.get('/', (req, res) => {
-    res.render('requestDashboard', {
-        loggedIn: true,
-        data: data,
-    });
-});
+router.get('/', RequestController.getAllRequests);
 
 // Create new Request
-router.get('/new', (req, res) => {
-    res.render('requestDetail', {
-        loggedIn: true,
-    });
-});
+router.get('/new', RequestController.createNewRequest);
 
 
 // View existing request
-router.get('/:id', (req, res) => {
-    res.render('requestDetail', {
-        loggedIn: true,
-    });
-});
+router.get('/:id', RequestController.getExistingRequest);
 
 module.exports = router;
