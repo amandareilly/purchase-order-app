03b3f2714a6ec74f2b0cea0c95b6079a1f00ae52
Amanda Reilly <amandamichellereilly@gmail.com>
2018-09-14 17:31:16 -0700

    Refactor apiRouter to use authMiddleware.

--- server/routes/apiRouter.js
+++ server/routes/apiRouter.js
@@ -8,53 +8,54 @@ const ItemApiController = require('../controllers/ItemApiController');
 const router = express.Router();
 const jsonParser = bodyParser.json();
 
-const { localAuth, jwtAuth } = require('../middleware/authentication');
+const { localAuth, jwtAuth, authMiddleware } = require('../middleware/authentication');
 
-let authMiddleware;
-if (process.env.NODE_ENV !== "TEST") {
-    authMiddleware = jwtAuth;
-} else {
-    authMiddleware = (req, res, next) => {
-        if (req.header('X-TEST-BYPASS-AUTH')) {
-            next();
-        } else {
-            jwtAuth(req, res, next);
-        }
-    };
-}
+// let authMiddleware;
+// if (process.env.NODE_ENV !== "TEST") {
+//     authMiddleware = jwtAuth;
+// } else {
+//     authMiddleware = (req, res, next) => {
+//         if (req.header('X-TEST-BYPASS-AUTH')) {
+//             next();
+//         } else {
+//             jwtAuth(req, res, next);
+//         }
+//     };
+// }
+router.use(jsonParser);
 router.use(authMiddleware)
     // requests endpoints
     // get all requests
-router.get('/requests', jwtAuth, PurchaseRequestApiController.getAllRequests);
+router.get('/requests', PurchaseRequestApiController.getAllRequests);
 // get a specific request
-router.get('/requests/:id', jsonParser, jwtAuth, PurchaseRequestApiController.getRequestById);
+router.get('/requests/:id', PurchaseRequestApiController.getRequestById);
 // save a new request
-router.post('/requests', jsonParser, jwtAuth, PurchaseRequestApiController.saveNewRequest);
+router.post('/requests', PurchaseRequestApiController.saveNewRequest);
 // update an existing request
-router.put('/requests/:id', jsonParser, jwtAuth, PurchaseRequestApiController.updateRequest);
+router.put('/requests/:id', PurchaseRequestApiController.updateRequest);
 // delete an existing request
-router.delete('/requests/:id', jsonParser, jwtAuth, PurchaseRequestApiController.deleteRequest);
+router.delete('/requests/:id', PurchaseRequestApiController.deleteRequest);
 
 // request item endpoints
 // add an item to a request
-router.post('/requests/:id/addItem', jsonParser, ItemApiController.addItem);
+router.post('/requests/:id/addItem', ItemApiController.addItem);
 // delete an item from a request
-router.delete('/requests/:id/item/:itemId', jsonParser, jwtAuth, ItemApiController.deleteItem);
+router.delete('/requests/:id/item/:itemId', ItemApiController.deleteItem);
 // update an item in a request
-router.put('/requests/:id/item/:itemId', jsonParser, jwtAuth, ItemApiController.updateItem);
+router.put('/requests/:id/item/:itemId', ItemApiController.updateItem);
 
 //user endpoints
 //get all users
-router.get('/users', jwtAuth, UserApiController.getAllUsers);
+router.get('/users', UserApiController.getAllUsers);
 //get a specific user by id
-router.get('/users/byId/:id', jsonParser, jwtAuth, UserApiController.getUserById);
+router.get('/users/byId/:id', UserApiController.getUserById);
 // get a specific user by email
-router.get('/users/byEmail/:email', jsonParser, jwtAuth, UserApiController.getUserByEmail);
+router.get('/users/byEmail/:email', UserApiController.getUserByEmail);
 //create a new user
-router.post('/users', jsonParser, jwtAuth, UserApiController.createUser);
+router.post('/users', UserApiController.createUser);
 
 // auth api endpoints
-router.post('/auth/login', localAuth, AuthController.issueToken);
-router.post('/auth/refresh', jwtAuth, AuthController.issueToken);
+router.post('/auth/login', AuthController.issueToken);
+router.post('/auth/refresh', AuthController.issueToken);
 
 module.exports = router;
