5249cbaf0ed9693a823264a15993780505931064
Amanda Reilly <amandamichellereilly@gmail.com>
2018-09-14 17:29:58 -0700

    Move authMiddleware function into authentication file.

--- server/middleware/authentication.js
+++ server/middleware/authentication.js
@@ -5,4 +5,19 @@ const jwtAuth = passport.authenticate('jwt', { session: false });
 
 const bearerAuth = passport.authenticate('bearer', { session: false });
 
-module.exports = { localAuth, jwtAuth, bearerAuth };
+let authMiddleware;
+
+authMiddleware = (req, res, next) => {
+    if (req.header('X-TEST-BYPASS-AUTH' || 'x-test-bypass-auth')) {
+        console.log('header found');
+        next();
+    } else if (req.originalUrl === '/api/auth/login') {
+        console.log('running local auth');
+        localAuth(req, res, next);
+    } else {
+        console.log('running jwtAuth');
+        jwtAuth(req, res, next);
+    }
+};
+
+module.exports = { localAuth, jwtAuth, bearerAuth, authMiddleware };
